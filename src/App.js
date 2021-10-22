0619ec76401a6a07b30937fd8aa66eb7c135e97d
LStitch <agsbale@hotmail.com>
2021-10-22 12:18:48 -0700

    Base Examen Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Hola mundo!!!!!.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+/*
+Librerias instaladas:
+npm install react-bootstrap bootstrap
+npm install --save prop-types
+npm install --save gh-pages
+
+Para eliminar el .git 
+rm -rf .git
+*/
