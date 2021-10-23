d5f2145f72fc0a0a9ec2bc7b94a9d8eb3d87a399
LStitch <agsbale@hotmail.com>
2021-10-22 19:49:38 -0700

    casi final commit

--- src/Components/Ticket.js
+++ src/Components/Ticket.js
@@ -12,7 +12,7 @@ const Ticket = (props) => {
                 <div className="Compra">
                   <p><strong>{props.compra.nombre}</strong> ({props.compra.idioma})</p>
                   <p><strong>Hora: </strong>{props.compra.horario}</p>
-                  <p><strong>Cantidad: </strong><input type='number' readOnly="" className="text-left" style={{width: '7vmin'}} value={props.compra.cantidad} min="1" max="6" onChange={e=>props.calcular(e.target.value,props.compra)}/></p>
+                  <p><strong>Cantidad: </strong><input type='number'  className="text-left" style={{width: '7vmin'}} value={props.compra.cantidad} min="1" max="6" onChange={e=>props.calcular(e.target.value,props.compra)}/></p>
                   <p><strong>Total: </strong>${(props.compra.total)}</p>
                   <div className="Boton">
                     <Button onClick={()=>props.eliminarCompra()}variant="danger">Cancelar</Button> 
